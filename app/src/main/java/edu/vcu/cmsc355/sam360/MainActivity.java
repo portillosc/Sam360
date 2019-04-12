0a15d3cf1bb6f9257b62111f986b785bab2679f2
Sam Portillo <portillosc@mymail.vcu.edu>
2019-04-12 19:59:14 -0400

    initial commit

---
+++ app/src/main/java/edu/vcu/cmsc355/sam360/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.vcu.cmsc355.sam360;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
